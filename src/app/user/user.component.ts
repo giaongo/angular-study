b0db683b19fd9b98a14b91f42ad0b6ab84f0f9c5
giaongo <giaongo.97@gmail.com>
2025-02-13 10:55:33 +0200

    implement signal input approach for user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input, computed} from '@angular/core';
 
 
 @Component({
@@ -8,12 +8,17 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath() {
+  avatar = input.required<string>()
+  name = input.required<string>()
+  imagePath = computed(() => {
+    return "assets/users/" + this.avatar()
+  })
+/*   get imagePath() {
     return "assets/users/" + this.avatar
-  }
+  } */
   onSelectUser() {
   }
 }
