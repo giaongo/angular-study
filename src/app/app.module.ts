e619bf056b25eb942f82317ec34102a27cc57644
giaongo <giaongo.97@gmail.com>
2025-02-17 19:09:57 +0200

    implement task modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,14 @@ import {NgModule} from "@angular/core"
 import { AppComponent } from "./app.component"
 import { HeaderComponent } from "./header/header.component"
 import { UserComponent } from "./user/user.component"
-import { TaskComponent } from "./tasks/task/task.component"
 import { BrowserModule } from "@angular/platform-browser"
-import { TasksComponent } from "./tasks/tasks.component"
-import { NewTaskComponent } from "./tasks/new-task/new-task.component"
-import { FormsModule } from "@angular/forms"
 import { SharedModule } from "./shared/shared.module"
+import { TasksModule } from "./tasks/tasks.module"
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule, SharedModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
