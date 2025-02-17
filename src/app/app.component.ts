2a98b8d7a2054bed6ba8e1f32b4492bedb89b29f
giaongo <giaongo.97@gmail.com>
2025-02-17 15:41:20 +0200

    implement module solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './user/dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
 import { User } from './user/user.model';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
